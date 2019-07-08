33a8ccd7695b05c86432e744cf87ed8c31b773cb
victor hugo <victor03016@gmail.com>
2019-07-08 22:21:55 +0200

    Creacion componente Clientes
    creacion de componente clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1 , nombre: 'Victor', apellido: 'Aguilar', email: 'Victor@gmail.com', createAt: '01/02/2019' },
+    {id: 2 , nombre: 'Paula', apellido: 'Rodriguez', email: 'Paula@gmail.com', createAt: '01/02/2019' },
+    {id: 3 , nombre: 'Eliana', apellido: 'Alvarez', email: 'Eliana@gmail.com', createAt: '01/02/2019' },
+    {id: 4 , nombre: 'Ricardo', apellido: 'Gomez', email: 'Ricardo@gmail.com', createAt: '01/02/2019' },
+    {id: 5 , nombre: 'Ricky', apellido: 'Manzur', email: 'Ricky@gmail.com', createAt: '01/02/2019' },
+    {id: 6 , nombre: 'Andrea', apellido: 'Tiron', email: 'Andrea@gmail.com', createAt: '01/02/2019' },
+    {id: 7 , nombre: 'Laura', apellido: 'Jacka', email: 'Laura@gmail.com', createAt: '01/02/2019' },
+    {id: 8 , nombre: 'Nahuel', apellido: 'Guanchi', email: 'Nahuel@gmail.com', createAt: '01/02/2019' },
+    {id: 9 , nombre: 'Thiago', apellido: 'Gonzales', email: 'Thiago@gmail.com', createAt: '01/02/2019' }
+];
