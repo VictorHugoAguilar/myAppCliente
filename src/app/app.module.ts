41b2773d2c26759d0ab3c042d66a8d72342931d6
victor hugo <victor03016@gmail.com>
2019-07-09 22:18:26 +0200

    implementacion de servicios por backend
    modificamos el servicio para traer servicios desde el backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+// Modulo para conexiones http
+import { HttpClientModule } from '@angular/common/http';
+
+// Modulo para las routas
+import { RouterModule, Routes } from "@angular/router";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { DirectivaComponent } from "./directiva/directiva.component";
 import { ClientesComponent } from "./clientes/clientes.component";
 import { ClienteService } from "./clientes/cliente.service";
-import { RouterModule, Routes } from "@angular/router";
 
 const ROUTES: Routes = [
     { path: "", redirectTo: "/clientes", pathMatch: "full" },
@@ -23,7 +28,11 @@ const ROUTES: Routes = [
         DirectivaComponent,
         ClientesComponent
     ],
-    imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(ROUTES)
+    ],
     providers: [ClienteService],
     bootstrap: [AppComponent]
 })
